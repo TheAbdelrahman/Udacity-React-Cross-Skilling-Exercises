a79f103f7f9c12f60834ffb8ac23fb729a4f0e0a
Abdelrahman <107592121+TheAbdelrahman@users.noreply.github.com>
2023-01-21 14:32:00 +0200

    L4 S.E cleanup

---
+++ 13 -L4 Side Effect Cleanup/src/ChildComponent.js
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from "react";
+
+function ChildComponent() {
+  const [color, setColor] = useState("red");
+
+  useEffect(() => {
+    let unmounted = false;
+
+    setTimeout(() => {
+      if(!unmounted){
+        setColor('green');
+      }
+  }, 3000);
+  return ()=>{
+    unmounted = true;
+  };
+  },[]);
+
+  return <p style={{ color }}>{color}</p>;
+}
+export default ChildComponent;
